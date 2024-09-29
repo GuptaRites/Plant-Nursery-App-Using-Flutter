bb155277f95341215f3d6e984b6e3505c43b9cf7
GuptaRites <rk7256967552@gmail.com>
2024-09-30 01:44:20 +0530

    Add Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:plant_nusery_app/screens/login.dart';
 
+import 'screens/splash_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -32,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(),
+      home: const SplashScreen(),
     );
   }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'login.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds:2), (){
+      Navigator.pushReplacement(context,
+      MaterialPageRoute(
+        builder: (context) => LoginPage(),
+        ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.green,
+        child: Center(
+          child: Text('Welcome',
+          style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+        ),
+        ),
+      ),
+    );
+  }
+}
